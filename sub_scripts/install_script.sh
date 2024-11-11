6e06c5f08c6928c3055ce6cd3fd451dca245d3ae
Neuron89 <hsnester@gmail.com>
2024-11-10 22:39:50 -0500

    this adds formating to the diff command to output a useable script.

--- sub_scripts/install_script.sh
+++ sub_scripts/install_script.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-package_list="/home/neuron/Documents/playbook_project/sub_scripts/pacman.txt"
-current_list="/home/neuron/Documents/playbook_project/sub_scripts/pacman_current.txt"
-missing="/home/neuron/Documents/playbook_project/tmp/missing_packages.txt"
+package_list="$HOME/Documents/playbook_project/package_info/pacman.txt"
+current_list="/$HOME/Documents/playbook_project/package_info/pacman_current.txt"
+missing="/$HOME/Documents/playbook_project/tmp/missing_packages.txt"
 
 #gets the current list of packages
 echo "getting current list of packages"
 pacman -Qqe > "${current_list}"
+sleep 1
 
 #compares the set list to the currently installed packages
 echo "comparing list of current packages"
-diff ${package_list} ${current_list} > "${missing}" 
+# sed is used to format the output of diff. s/^[<>]// is used to replace anything in the [] with anything inbetween the //. s/^[ \t]// is used to replace any empty spaces at the begining of the line to keep formating clean. the G at the end is to apply this globally. the following sed is to remove the numbers created on the list. 1d deletes he first line; then we skip the next with n; and then delete with d. that will repeat all the way down.
+diff ${package_list} ${current_list} | sed 's/^[<>]//;s/^[ \t]//g' | sed '1d; n; d' > "${missing}" 
+sleep 1
 
 #displays missing packages
 echo "missing packages:"
 cat ${missing}
+sleep 1
 
+#statement to ask if you want to install the packages
 while true; do
     read -p "Install missing packages? (y/n): " yn
     case $yn in
@@ -31,5 +36,10 @@ while true; do
     esac
 done
 
-rm /home/neuron/Documents/playbook_project/tmp/missing_packages.txt
+#deletes the temp file 
+rm "${missing}"
+
+#updating all packages
+sudo pacman -Syu --noconfirm
+
 
